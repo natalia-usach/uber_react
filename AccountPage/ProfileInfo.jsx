a621b2e231b6c9ebb4fd1481754d5b118f06596a
Natalia Usach <klimchuknatalia@gmail.com>
2021-10-21 09:10:43 +0000

    Upload New File

---
+++ AccountPage/ProfileInfo.jsx
@@ -0,0 +1,9 @@
+const ProfileInfo = (props) => {
+    return (
+        <div className='profile' onClick={props.clickEvent}>
+            Profile
+        </div>
+    );
+}
+
+export default ProfileInfo;
