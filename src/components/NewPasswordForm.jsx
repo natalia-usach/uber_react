cf6f4d28ab8e1514193acb8ece9c95b6e2f967e7
natalia-usach <58885439+natalia-usach@users.noreply.github.com>
2021-10-21 22:09:29 +0300

    Update NewPasswordForm.jsx

--- src/components/NewPasswordForm.jsx
+++ src/components/NewPasswordForm.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
 import Input from "./Input";
 import { sendNewPassword } from "../actions/user";
 
 const NewPasswordForm = () => {
 
     const [email, setEmail] = useState('');
-    const dispatch = useDispatch();
+    const [message, setMessage] = useState('');
 
+    const getMessageInfo = async () => {
+        const message = await sendNewPassword(email);
+        setMessage(message);
+    }
     return (
-        <div className='auth'>
-            <p>Please enter your email so that we could send you a new password</p>
-            <Input value={email} setValue={setEmail} type='email' placeholder='Email'/>
-            <button className='authBtn' onClick={() => dispatch(sendNewPassword(email))}>Send</button>
+        <div>
+            {!message && <div className='auth'>
+                <p>Please enter your email so that we could send you a new password</p>
+                <Input value={email} setValue={setEmail} type='email' placeholder='Email'/>
+                <button className='authBtn' onClick={getMessageInfo}>Send</button>
+            </div>}
+            {message && <div className='auth'>{message}</div>}
         </div>
     );
 }
